3eea3c0baf4bbf107752888fc1e62e7858a332bb
Yoann Leonard <yoann.leonard@gmail.com.com>
2022-07-15 18:17:26 +0200

    added display to details page

--- src/Controller/TrickController.php
+++ src/Controller/TrickController.php
@@ -4,24 +4,42 @@ namespace App\Controller;
 
 use App\Entity\Trick;
 use App\Entity\User;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
-use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
-use Symfony\Component\Form\Extension\Core\Type\TextareaType;
-use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use App\Entity\Comment;
+use App\Form\Type\TrickType;
+use App\Form\Type\CommentType;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Component\String\Slugger\AsciiSlugger;
+
+use function PHPUnit\Framework\isEmpty;
 
 class TrickController extends AbstractController
 {
     /**
      * @Route("/trick/details/{slug}", name="trick_details")
      */
-    public function details(): Response
+    public function details(string $slug, ManagerRegistry $doctrine, Request $request): Response
     {
-        return $this->render('trick/details.html.twig');
+       
+        $trick = $doctrine->getRepository(Trick::class)->findOneBy(['slug' => $slug]);
+        dump($trick);
+
+        // TODO get current logged user
+        $user = $doctrine->getRepository(User::class)->find(1);
+
+        $comment = new Comment();
+        $commentForm = $this->createForm(CommentType::class, $comment);
+        $commentForm->handleRequest($request);
+        $commentForm->createView();
+        
+        return $this->render('trick/details.html.twig', [
+            'trick' => $trick,
+            'user' => $user,
+            'commentForm' => $commentForm->createView(),
+        ]);
     }
 
     /**
@@ -29,47 +47,34 @@ class TrickController extends AbstractController
      */
     public function create(ManagerRegistry $doctrine, Request $request): Response
     {
+        // TODO get current logged user
+        $user = $doctrine->getRepository(User::class)->find(1);
+        if (empty($user)) return $this->redirectToRoute('error_you_have_to_be_logged_in');
+
         $trick = new Trick();
-        
-        $form = $this->createFormBuilder($trick)
-            ->add('name', TextType::class)
-            ->add('description', TextareaType::class)
-            ->add('category', ChoiceType::class, [
-                'choices' => [
-                    'jump' => 'jump',
-                    'rotation' => 'rotation',
-                    'press' => 'press',
-                    'grab' => 'grab',
-                    'rail' => 'rail',
-                ]
-            ])
-            ->add('save', SubmitType::class, ['label' => 'Create Trick'])
-            ->getForm();
-
+        $form = $this->createForm(TrickType::class, $trick);
         $form->handleRequest($request);
+
         if ($form->isSubmitted() && $form->isValid()) {
-            // $form->getData() holds the submitted values
-            // but, the original `$task` variable has also been updated
+
             $trick = $form->getData();
-
-            $user = $doctrine->getRepository(User::class)->find(1);
-            $tricksRepository = $doctrine->getRepository(Trick::class);
-
             $trick->setUser($user);
             $trick->setDate(new \DateTime());
-
+            $slugger = new AsciiSlugger();
+            $trick->setSlug($slugger->slug($trick->getName()));
+            
+            $tricksRepository = $doctrine->getRepository(Trick::class);
             $trickAlreadyInDB = $tricksRepository->findOneBy(['name' => $trick->getName()]);
 
             if ($trickAlreadyInDB) {
                 return $this->redirectToRoute('error_name_already_used');
             }
 
-
             $entityManager = $doctrine->getManager();
             $entityManager->persist($trick);
             $entityManager->flush();
 
-            return $this->redirectToRoute('trick_details', ['slug' => $trick->getName()]);
+            return $this->redirectToRoute('trick_details', ['slug' => $trick->getSlug()]);
         }
 
         return $this->render('trick/create.html.twig', [
