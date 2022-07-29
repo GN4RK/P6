e228f7897c33ffd427200dd1016c9987f4df70f7
Yoann Leonard <yoann.leonard@gmail.com.com>
2022-07-29 20:14:18 +0200

    deleted unused code

--- src/Form/Type/ImageType.php
+++ src/Form/Type/ImageType.php
@@ -2,8 +2,6 @@
 namespace App\Form\Type;
 
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
-use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
