ca649afd8047038259a4a2569bcc132f7706be32
Yoann Leonard <yoann.leonard@gmail.com.com>
2022-07-30 18:15:50 +0200

    added featured image to ajax queries

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -36,6 +36,9 @@ class HomeController extends AbstractController
                 $response[] = array (
                     'name' => $trick->getName(),
                     'slug' => $trick->getSlug(),
+                    'featuredImage' => ($trick->getFeaturedImage() != null) ? 
+                        $trick->getFeaturedImage()->getContent() 
+                        : null
                 );
             }
 
