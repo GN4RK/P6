2d178c90d1b0c07f67471b6af763a707995e9b14
Yoann Leonard <yoann.leonard@gmail.com.com>
2022-08-03 15:55:28 +0200

    added new form type

--- src/Form/Type/ResetPasswordType.php
+++ src/Form/Type/ResetPasswordType.php
@@ -3,19 +3,18 @@ namespace App\Form\Type;
 
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
-use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 
-class SignUpType extends AbstractType
+class ResetPasswordType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
             ->add('username', TextType::class)
-            ->add('email', EmailType::class)
-            ->add('password', PasswordType::class)
-            ->add('save', SubmitType::class, ['label' => 'Create an account']);
+            ->add('new_password', PasswordType::class)
+            ->add('confirm', PasswordType::class)
+            ->add('save', SubmitType::class, ['label' => 'Reset password']);
     }    
 }
